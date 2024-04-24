940e7124a3d36214c5536f09f234dcfb9199e635
satishsurani <153635871+satishsurani@users.noreply.github.com>
2024-04-24 15:57:50 +0530

    enter add

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const checkBtn = document.getElementById("check-btn");
 const result = document.getElementById("result");
 
-checkBtn.addEventListener("click", () => {
+function palcheck(){
     const textInput = document.getElementById("text-input").value;
     const lowerCaseStr = textInput.replace(/[^A-Za-z0-9]/gi, '').toLowerCase();
     if (textInput === "") {
@@ -10,5 +10,11 @@ checkBtn.addEventListener("click", () => {
     else {
         lowerCaseStr === [...lowerCaseStr].reverse().join('') ? result.innerHTML = `${textInput} is a palindrome.` : result.innerHTML = `${textInput} is not a palindrome`
         
+    }
+}
+
+document.getElementById('text-input').addEventListener("keypress", (e)=>{
+    if (e.key === 'Enter') {
+        palcheck()
     }
 })
